5725a7a866f2172c2514f1b477e84ed1b3184615
mersious <mersadwar2000@gmail.com>
2024-05-01 17:25:58 +0330

    feat(examples): Add RTLS code between a Tag & Anchor

---
+++ examples/RTLS_Anchor/RTLS_Anchor.ino
@@ -0,0 +1,116 @@
+/* 
+ * StandardRTLSAnchorMain_TWR.ino
+ * 
+ * This is an example master anchor in a RTLS using two way ranging ISO/IEC 24730-62_2013 messages
+ */
+
+#include <DW1000Ng.hpp>
+#include <DW1000NgUtils.hpp>
+#include <DW1000NgRanging.hpp>
+#include <DW1000NgRTLS.hpp>
+
+// connection pins
+#if defined(ESP8266)
+const uint8_t PIN_RST = D1; // reset pin
+const uint8_t PIN_IRQ = D2; // irq pin
+const uint8_t PIN_SS = SS; // spi select pin
+#else
+const uint8_t PIN_RST = 9;
+const uint8_t PIN_SS = SS; // spi select pin
+#endif
+
+// Extended Unique Identifier register. 64-bit device identifier. Register file: 0x01
+const char EUI[] = "AA:BB:CC:DD:EE:FF:00:01";
+
+double range_self;
+
+byte target_eui[8];
+byte tag_shortAddress[] = {0x05, 0x00};
+
+uint16_t blink_rate = 200;
+
+device_configuration_t DEFAULT_CONFIG = {
+    false,
+    true,
+    true,
+    true,
+    false,
+    SFDMode::STANDARD_SFD,
+    Channel::CHANNEL_5,
+    DataRate::RATE_850KBPS,
+    PulseFrequency::FREQ_16MHZ,
+    PreambleLength::LEN_256,
+    PreambleCode::CODE_3
+};
+
+frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+    false,
+    true /* This allows blink frames */
+};
+
+void setup() {
+    // DEBUG monitoring
+    Serial.begin(115200);
+    Serial.println(F("### DW1000Ng-arduino-ranging-anchorMain ###"));
+    // initialize the driver
+    #if defined(ESP8266)
+    DW1000Ng::initializeNoInterrupt(PIN_SS);
+    #else
+    DW1000Ng::initializeNoInterrupt(PIN_SS, PIN_RST);
+    #endif
+    Serial.println(F("DW1000Ng initialized ..."));
+    // general configuration
+    DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
+    DW1000Ng::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
+    
+    DW1000Ng::setEUI(EUI);
+
+    DW1000Ng::setPreambleDetectionTimeout(64);
+    DW1000Ng::setSfdDetectionTimeout(273);
+    DW1000Ng::setReceiveFrameWaitTimeoutPeriod(2000);
+
+    DW1000Ng::setNetworkId(RTLS_APP_ID);
+    DW1000Ng::setDeviceAddress(1);
+	
+    DW1000Ng::setAntennaDelay(16436);
+    
+    Serial.println(F("Committed configuration ..."));
+    // DEBUG chip info and registers pretty printed
+    char msg[128];
+    DW1000Ng::getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
+    DW1000Ng::getPrintableDeviceMode(msg);
+    Serial.print("Device mode: "); Serial.println(msg);    
+}
+
+void loop() {
+    if(DW1000NgRTLS::receiveFrame()){
+        size_t recv_len = DW1000Ng::getReceivedDataLength();
+        byte recv_data[recv_len];
+        DW1000Ng::getReceivedData(recv_data, recv_len);
+
+
+        if(recv_data[0] == BLINK) {
+            DW1000NgRTLS::transmitRangingInitiation(&recv_data[2], tag_shortAddress);
+            DW1000NgRTLS::waitForTransmission();
+
+            RangeAcceptResult result = DW1000NgRTLS::anchorRangeAccept(NextActivity::ACTIVITY_FINISHED, blink_rate);
+            if(!result.success) return;
+            range_self = result.range;
+
+            String rangeString = "Range: "; rangeString += range_self; rangeString += " m";
+            rangeString += "\t RX power: "; rangeString += DW1000Ng::getReceivePower(); rangeString += " dBm";
+            Serial.println(rangeString);
+        }
+    }
+}
